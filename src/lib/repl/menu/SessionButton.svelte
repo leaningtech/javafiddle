94c102e44d37c79f36c6f48aa47c953959c057d8
Alex Bates <alex@nanaian.town>
2023-09-13 10:32:39 +0100

    style login/logout buttons

--- src/lib/repl/menu/SessionButton.svelte
+++ src/lib/repl/menu/SessionButton.svelte
@@ -8,11 +8,11 @@
 </script>
 
 {#if isLoggedIn}
-	<a href="/auth/logout">
+	<a href="/auth/logout" class="text-sm rounded bg-stone-100 hover:bg-stone-200 font-semibold px-3 py-2">
 		Sign out
 	</a>
 {:else}
-	<a href={githubLoginUrl}>
+	<a href={githubLoginUrl} class="text-sm rounded bg-stone-100 hover:bg-stone-200 font-semibold px-3 py-2">
 		Sign in
 	</a>
 {/if}
