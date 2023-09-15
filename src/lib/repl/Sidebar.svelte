962efaa3e3290b2f464400c91b4fe9f675f8d7ab
Alex Bates <alex@nanaian.town>
2023-09-15 12:26:43 +0100

    fade javafiddle info text

--- src/lib/repl/Sidebar.svelte
+++ src/lib/repl/Sidebar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Icon from "@iconify/svelte";
 	import { isSidebarOpen } from "./state";
+	import { fade } from 'svelte/transition';
 </script>
 
 <aside class="w-10 bg-gray-100 dark:bg-gray-800 flex-shrink-0 transition-[width]" class:!w-80={$isSidebarOpen}>
@@ -14,23 +15,25 @@
 		</button>
 	</div>
 
-	<div class="w-80 p-4 leading-tight text-gray-700 dark:text-gray-400 text-sm">
-		<p>
-			JavaFiddle is an online tool to test and share snippets of Java code.
-		</p>
+	{#if $isSidebarOpen}
+		<div class="w-80 p-4 leading-tight text-gray-700 dark:text-gray-400 text-sm" transition:fade={{ duration: 150 }}>
+			<p>
+				JavaFiddle is an online tool to test and share snippets of Java code.
+			</p>
 
-		<hr class="my-4 dark:border-gray-700" />
+			<hr class="my-4 dark:border-gray-700" />
 
-		<ul class="list-disc space-y-2 ml-4">
-			<li>
-				Runs entirely <b>in your browser</b>.
-			</li>
-			<li>
-				Works <b>offline</b>.
-			</li>
-			<li>
-				Supports Swing, audio, printing - any Java SE component.
-			</li>
-		</ul>
-	</div>
+			<ul class="list-disc space-y-2 ml-4">
+				<li>
+					Runs entirely <b>in your browser</b>.
+				</li>
+				<li>
+					Works <b>offline</b>.
+				</li>
+				<li>
+					Supports Swing, audio, printing - any Java SE component.
+				</li>
+			</ul>
+		</div>
+	{/if}
 </aside>
