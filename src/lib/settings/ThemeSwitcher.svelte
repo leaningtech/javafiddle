af7d7588f5e0ca4a712274f7d33c38379d834dfb
Alex Bates <alex@nanaian.town>
2023-09-18 12:38:17 +0100

    use icons for theme switcher instead of select

--- src/lib/settings/ThemeSwitcher.svelte
+++ src/lib/settings/ThemeSwitcher.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import { theme } from "./store";
+	import { theme, effectiveTheme } from "./store";
+	import Icon from "@iconify/svelte";
+
+	function toggle() {
+		$theme = ($effectiveTheme === "dark" ? "light" : "dark");
+	}
 </script>
 
-<select bind:value={$theme}>
-	<option value="system">System</option>
-	<option value="light">Light</option>
-	<option value="dark">Dark</option>
-</select>
+<button class="p-1 text-gray-500 hover:text-gray-700 dark:hover:text-gray-400" on:click={toggle} aria-label="Toggle dark theme">
+	<Icon icon={$effectiveTheme === "dark" ? "mi:moon" : "mi:sun"} class="w-5 h-5" />
+</button>
