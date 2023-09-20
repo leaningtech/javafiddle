532aa4a3f61333547d9f910933bf117597e4611a
Alex Bates <alex@nanaian.town>
2023-09-20 11:30:47 +0100

    remove SessionButton

--- src/lib/repl/Menu.svelte
+++ src/lib/repl/Menu.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import SessionButton from "./menu/SessionButton.svelte";
 	import { createEventDispatcher } from "svelte";
 	import Icon from '@iconify/svelte';
 	import { relativeTime } from 'svelte-relative-time';
@@ -7,10 +6,12 @@
 	import FiddleTitle from "./menu/FiddleTitle.svelte";
 	import SettingsButton from "./menu/SettingsButton.svelte";
 	import { autoRun } from "$lib/settings/store";
+	import { PUBLIC_GITHUB_CLIENT_ID } from "$env/static/public";
 
 	$: isLoggedIn = typeof $page.data.session.userId === "number";
 
 	const dispatch = createEventDispatcher<{ save: undefined, run: undefined }>();
+	const githubLoginUrl = `https://github.com/login/oauth/authorize?client_id=${PUBLIC_GITHUB_CLIENT_ID}`;
 
 	export let updated: Date | undefined;
 	export let isSaving = false;
@@ -34,11 +35,11 @@
 
 	<div class="grow flex flex-col justify-center self-stretch">
 		<FiddleTitle />
-		<div class="h-4 leading-3 text-xs text-gray-500 dark:text-gray-400">
-			{#if updated}
+		{#if updated}
+			<div class="h-4 leading-3 text-xs text-gray-500 dark:text-gray-400">
 				<span use:relativeTime={{ date: updated }} />
-			{/if}
-		</div>
+			</div>
+		{/if}
 	</div>
 
 	<ul class="flex items-center gap-2">
@@ -67,10 +68,12 @@
 					Save
 				</button>
 			</li>
+		{:else}
+			<a href={githubLoginUrl} class="text-sm rounded bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 font-semibold px-3 py-1 h-8 flex items-center">
+				<Icon icon="mi:cloud-upload" class="w-5 h-5 mr-1" />
+				Log in to save
+			</a>
 		{/if}
-		<li>
-			<SessionButton />
-		</li>
 		<li>
 			<SettingsButton />
 		</li>
