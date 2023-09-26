2d6ed1b54eac047bd96271907fd4399ad2f6770d
Alex Bates <alex@nanaian.town>
2023-09-26 16:06:51 +0100

    set meta description to file content

--- src/routes/[[id]]/+layout.svelte
+++ src/routes/[[id]]/+layout.svelte
@@ -6,10 +6,13 @@
 	$: $files = data.fiddle.files;
 	$: $fiddleTitle = data.fiddle.title;
 	$: $fiddleUpdated = data.fiddle.updated;
+
+	const description = $files[0].content;
 </script>
 
 <svelte:head>
 	<title>{$fiddleTitle ? `${$fiddleTitle} - JavaFiddle` : "JavaFiddle - Build and share Java code snippets in your browser"}</title>
+	<meta name="description" content={description}>
 </svelte:head>
 
 <slot />
