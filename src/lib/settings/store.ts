c0e53ecc975972919438e83db49e9de9b75f6def
Alex Bates <alex@nanaian.town>
2023-09-27 13:59:36 +0100

    add theme override query param

--- src/lib/settings/store.ts
+++ src/lib/settings/store.ts
@@ -6,6 +6,13 @@ export type Theme = "system" | "light" | "dark";
 export const theme = persist(writable<Theme>("system"), createLocalStorage(), "theme");
 
 export const effectiveTheme = derived(theme, ($theme) => {
+	if (browser) {
+		const params = new URLSearchParams(document.location.search);
+		if (params.has("theme")) {
+			return params.get("theme");
+		}
+	}
+
 	if ($theme === "system") {
 		return (browser && window.matchMedia("(prefers-color-scheme: light)").matches) ? "light" : "dark";
 	}
