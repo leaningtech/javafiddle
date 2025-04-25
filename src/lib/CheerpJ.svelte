222977e51c4a6cb0848c0fac5f73880668c6709b
Francesco <francesco@leaningtech.com>
2025-04-25 13:13:37 +0200

    CheerpJ loader now is called directly from path stored in .env

--- src/lib/CheerpJ.svelte
+++ src/lib/CheerpJ.svelte
@@ -1,31 +1,12 @@
 <script lang="ts">
-	import { PUBLIC_CHEERPJ_ORIGIN } from '$env/static/public';
-	import { onMount, createEventDispatcher } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
+	import { browser } from '$app/environment';
 
 	const dispatch = createEventDispatcher<{ ready: undefined }>();
 
 	export let display: HTMLElement;
 
-	let promise: Promise<string | void> = new Promise(() => {});
-	let error = false;
-
-	// Load build of CheerpJ by getting url from LATEST.txt and placing it in script tag
-	onMount(() => {
-		promise = fetch(`${PUBLIC_CHEERPJ_ORIGIN}/LATEST.txt`)
-			.then((res) => res.text())
-			.then((text) => text.trim())
-			.catch((err) => {
-				console.error('Error loading CheerpJ', err);
-				error = true;
-			});
-	});
-
 	async function onLoad() {
-		// CheerpJ funcs should be available now
-		if (!cheerpjInit || !display) {
-			error = true;
-			return;
-		}
 
 		await cheerpjInit({
 			status: 'none',
@@ -34,18 +15,8 @@
 		cheerpjCreateDisplay(-1, -1, display);
 		dispatch('ready');
 	}
+
+	if (browser) {	// so it doesn't run server-side
+		onLoad();
+	}
 </script>
-
-<svelte:head>
-	{#await promise}
-		<!-- CheerpJ is loading -->
-	{:then src}
-		{#if src}
-			<script {src} on:load={onLoad}></script>
-		{/if}
-	{/await}
-</svelte:head>
-
-{#if error}
-	<strong class="p-3">Error loading CheerpJ</strong>
-{/if}
