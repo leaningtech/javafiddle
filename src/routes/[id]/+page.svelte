6240d7572a39d265c82081b98b9496f8772e33ce
Francesco <francesco@leaningtech.com>
2025-05-07 13:27:32 +0200

    added support for legacy URLs

---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	// to support legacy URLs like https://javafiddle.leaningtech.com/PARAM
+	// the parameter is converted into fragment before the redirection
+	export let data;
+	const encodedParamURL = data.id;
+	if (browser) {
+		goto(`/#${encodedParamURL}`);
+	}
+</script>
---
+++ src/routes/[id]/+page.ts
@@ -0,0 +1,5 @@
+export function load({ params }) {
+	return {
+		id: params.id
+	};
+}
