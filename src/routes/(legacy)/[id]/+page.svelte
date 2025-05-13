4a66c9cf886899ea007f464d7f35a2c5717f99a2
Francesco <francesco@leaningtech.com>
2025-05-13 18:30:37 +0200

    legacy URLs like /<param>/embed or /<param>/output are now supported

---
+++ src/routes/(legacy)/[id]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	// to support legacy URLs like https://javafiddle.leaningtech.com/PARAM[/theme=dark|light]
+	// the parameter is converted into fragment before the redirection
+	if (browser) {
+		let redirectTo: string;
+		const query = $page.url.search;
+		const encodedParamURL = $page.params.id;
+		if (query) {
+			redirectTo = `/${query}#${encodedParamURL}`
+		} else {
+			redirectTo = `/#${encodedParamURL}`
+		}
+		goto(redirectTo);
+	}
+</script>
---
+++ src/routes/(legacy)/[id]/output/+page.svelte
@@ -0,0 +1,17 @@
+<script lang='ts'>
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+
+	if (browser) {
+		let redirectTo: string;
+		const query = $page.url.search;
+		const encodedParamURL = $page.params.id;
+		if (query) {
+			redirectTo = `/output${query}#${encodedParamURL}`
+		} else {
+			redirectTo = `/output#${encodedParamURL}`
+		}
+		goto(redirectTo);
+	}
+</script>
