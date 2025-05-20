0a5c7464afe65061dafc2c76ba13de9bdd669f65
Francesco <francesco@leaningtech.com>
2025-05-21 01:46:52 +0200

    removed iframe

--- src/routes/(app)/embed/+page.ts
+++
@@ -1,8 +0,0 @@
-export function load({ url }) {
-	let outputUrl = url.pathname.replace('/embed', '');
-	if (!outputUrl.endsWith('/')) outputUrl += '/';
-	outputUrl += 'output';
-	return {
-		outputUrl
-	};
-}
