45e932dc83fe9be4a431176c9565a80e331d0afb
Francesco <francesco@leaningtech.com>
2025-06-02 17:46:03 +0200

    dropped iframe

--- src/routes/(app)/embed/+page.svelte
+++ src/routes/(app)/embed/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import Repl from '$lib/Repl.svelte';
-
-	export let data;
 </script>
 
-<Repl outputUrl={data.outputUrl} enableMenu={false} enableSidebar={false} />
+<Repl enableMenu={false} enableSidebar={false} />
