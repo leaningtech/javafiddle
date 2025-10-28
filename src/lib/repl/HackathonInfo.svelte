2a6fc84319a23e87d59740346cf96a6e7e3d27cb
cidungfrog27 <codingmerlin@gmail.com>
2025-10-28 19:40:26 +0100

    fixed light mode colours but found weird sidebar closing bug

--- src/lib/repl/HackathonInfo.svelte
+++ src/lib/repl/HackathonInfo.svelte
@@ -1,7 +1,7 @@
-<div class="hack-info">
+<div class="hack-info border-[5px] border-[#fe900d] border-groove text-center p-3 m-[2px] text-black box-border dark:text-white">
 	<h2 class="text-lg font-bold mb-2">CheerpJ Hackathon</h2>
 	<p>
-		Join our first <a href="https://labs.leaningtech.com/cheerpj-hackathon" target="_blank">CheerpJ Hackathon</a> 
+		Join our first <a href="https://labs.leaningtech.com/cheerpj-hackathon" target="_blank" class="text-orange-500 dark:text-orange-400 underline">CheerpJ Hackathon</a> 
 		in the second half of November, exploring how <b>CheerpJ</b> can be applied to real-world science and research projects, with a <b>£500</b> prize awaiting the winning team. Join our <a href="https://discord.leaningtech.com" target="_blank">Discord</a> for further updates!
 	</p>
 
