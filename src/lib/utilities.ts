76514530f8f7ade9c51e945a9cc2b3a03ee51430
cidungfrog27 <codingmerlin@gmail.com>
2025-10-30 11:19:56 +0100

    prettier additions

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -1,13 +1,12 @@
 // for adblockers protection
 export function tryPlausible(msg: string) {
-	if (self.plausible)
-		plausible(msg)
+	if (self.plausible) plausible(msg);
 }
 
 export function debounceFunction(fn: () => void, delay: number) {
 	let timeoutId: number;
-	return(() => {
+	return () => {
 		clearInterval(timeoutId);
 		timeoutId = setTimeout(() => fn(), delay);
-	});
+	};
 }
